27fc424c19bbb42c86977fa5e7aa1b6002467e19
Grant Eben <gmeben@gmail.com>
2025-06-23 19:57:24 -0400

    refactor: Make Bibliography a component

---
+++ wp-content/plugins/kcworks/src/kcworks/components/Bibliography.js
@@ -0,0 +1,87 @@
+import { __ } from "@wordpress/i18n";
+import { Card, CardBody } from "@wordpress/components";
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { useBlockProps } from "@wordpress/block-editor";
+
+const Bibliography = ({
+    fetchError,
+    dataFetched,
+    bibliography,
+    kcworksQuery,
+    loading,
+    results,
+}) => {
+    return (
+        <div {...useBlockProps()}>
+            {(!kcworksQuery || !loading) && !dataFetched && (
+                <div role="alert">
+                    <Card>
+                        <CardBody>
+                            <p>
+                                {__(
+                                    "Please set a query to fetch data from KCWorks.",
+                                    "kcworks",
+                                )}
+                            </p>
+                        </CardBody>
+                    </Card>
+                </div>
+            )}
+            {loading && (
+                <div role="alert">
+                    <Card>
+                        <CardBody>
+                            <p>
+                                {__("Loading data from KCWorks...", "kcworks")}
+                            </p>
+                        </CardBody>
+                    </Card>
+                </div>
+            )}
+            {dataFetched && results.length === 0 && (
+                <div role="alert">
+                    <Card>
+                        <CardBody>
+                            <p>
+                                {__("No results for KCWorks query.", "kcworks")}
+                            </p>
+                        </CardBody>
+                    </Card>
+                </div>
+            )}
+            {dataFetched && results.length > 0 && (
+                <section className="kcworks-bibliography">
+                    {
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: bibliography,
+                            }}
+                        />
+                    }
+                </section>
+            )}
+            {fetchError && (
+                <div role="alert">
+                    <Card>
+                        <CardBody>
+                            <p>
+                                {__(
+                                    "An error occurred while fetching the data from KCWorks",
+                                    "kcworks",
+                                )}
+                            </p>
+                        </CardBody>
+                    </Card>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Bibliography;
--- wp-content/plugins/kcworks/src/kcworks/edit.js
+++ wp-content/plugins/kcworks/src/kcworks/edit.js
@@ -3,16 +3,10 @@
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
  */
-import { __ } from '@wordpress/i18n';
-import { useCallback, useEffect, useState } from '@wordpress/element';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { useBlockProps } from '@wordpress/block-editor';
+import { __ } from "@wordpress/i18n";
+import { useCallback, useEffect, useState } from "@wordpress/element";
+import apiFetch from "@wordpress/api-fetch";
+import { addQueryArgs } from "@wordpress/url";
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -20,49 +14,43 @@ import { useBlockProps } from '@wordpress/block-editor';
  *
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
-import './editor.scss';
-import { Card, CardBody } from '@wordpress/components';
-import DataBlockInspectorControls from './components/DataBlockInspectorControls.js';
-import apiFetch from '@wordpress/api-fetch';
-import { addQueryArgs } from '@wordpress/url';
-import { getItems, sortItems } from './processdata.js';
-import CSL from 'citeproc';
-import locales from './locales/locales.json';
+import "./editor.scss";
+import DataBlockInspectorControls from "./components/DataBlockInspectorControls.js";
+import Bibliography from "./components/Bibliography.js";
+import CSL from "citeproc";
+import locales from "./locales/locales.json";
+import { getItems, sortItems } from "./processdata.js";
 
-const pluginBaseUrl = '/wp-content/plugins/kcworks/src/kcworks';
+const pluginBaseUrl = "/wp-content/plugins/kcworks/src/kcworks";
 
 /**
  * @param {string} styleId
  * @returns
  */
-function getCslFileStyle( styleId ) {
-	if ( styleId === null ) {
-		return '';
-	}
-	const xhr = new XMLHttpRequest();
-	xhr.open( 'GET', `${ pluginBaseUrl }/styles/${ styleId }.csl`, false );
-	xhr.send( null );
-	const text = xhr.responseText;
-	const parser = new DOMParser();
-	return parser.parseFromString( text, 'text/xml' );
+function getCslFileStyle(styleId) {
+    if (styleId === null) {
+        return "";
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", `${pluginBaseUrl}/styles/${styleId}.csl`, false);
+    xhr.send(null);
+    const text = xhr.responseText;
+    const parser = new DOMParser();
+    return parser.parseFromString(text, "text/xml");
 }
 
 /**
  * @param {string} localeId
  * @returns
  */
-function getXmlFileLocale( localeId ) {
-	if ( localeId === null ) {
-		return null;
-	}
-	const xhr = new XMLHttpRequest();
-	xhr.open(
-		'GET',
-		`${ pluginBaseUrl }/locales/locales-${ localeId }.xml`,
-		false
-	);
-	xhr.send( null );
-	return xhr.responseText;
+function getXmlFileLocale(localeId) {
+    if (localeId === null) {
+        return null;
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", `${pluginBaseUrl}/locales/locales-${localeId}.xml`, false);
+    xhr.send(null);
+    return xhr.responseText;
 }
 
 /**
@@ -73,137 +61,140 @@ function getXmlFileLocale( localeId ) {
  *
  * @return {Element} Element to render.
  */
-export default function Edit( { attributes, setAttributes } ) {
-	const { kcworksQuery, validatedKcworksQuery } = attributes;
-	const [ dataFetched, setDataFetched ] = useState( false );
-	const [ results, setResults ] = useState( [] );
-	const [ invalidQuery, setInvalidQuery ] = useState( false );
-	const [ loading, setLoading ] = useState( true );
-	const [ fetchError, setFetchError ] = useState( false );
-	const [ styleSetting, setStyleSetting ] = useState( 'apa' );
-	const [ localeSetting, setLocaleSetting ] = useState( 'en-US' );
-	const [ sortSetting, setSortSetting ] = useState( 'newest' );
-	const [ bibliography, setBibliography ] = useState( '<p>...</p>' );
+export default function Edit({ attributes, setAttributes }) {
+    const { kcworksQuery, validatedKcworksQuery } = attributes;
+    const [dataFetched, setDataFetched] = useState(false);
+    const [results, setResults] = useState([]);
+    const [invalidQuery, setInvalidQuery] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [fetchError, setFetchError] = useState(false);
 
-	const fetchData = useCallback( async () => {
-		setFetchError( false );
-		setDataFetched( false );
+    const [styleSetting, setStyleSetting] = useState("apa");
+    const [localeSetting, setLocaleSetting] = useState("en-US");
+    const [sortSetting, setSortSetting] = useState("newest");
+    const [bibliography, setBibliography] = useState("<p>...</p>");
 
-		const queryParams = { kcworksQuery: `${ kcworksQuery }` };
-		apiFetch( {
-			path: addQueryArgs(
-				'/mesh_research_kcworks/v1/kcworks-proxy',
-				queryParams
-			),
-		} )
-			.then( ( data ) => {
-				setDataFetched( true );
-				let a = sortItems( getItems( data ) );
-				setResults( a );
-			} )
-			.catch( ( error ) => {
-				console.error( error );
-				setFetchError( true );
-			} )
-			.finally( () => {
-				setLoading( false );
-			} );
-	}, [ kcworksQuery ] );
+    const fetchData = useCallback(
+        async (
+            setFetchError,
+            setDataFetched,
+            kcworksQuery,
+            setResults,
+            setLoading,
+        ) => {
+            setFetchError(false);
+            setDataFetched(false);
 
-	useEffect( () => {
-		if ( kcworksQuery && validatedKcworksQuery && ! dataFetched ) {
-			fetchData();
-		}
-		if ( ! kcworksQuery ) {
-			setLoading( false );
-		}
-	}, [ kcworksQuery, validatedKcworksQuery, dataFetched, fetchData ] );
+            const queryParams = { kcworksQuery: `${kcworksQuery}` };
+            apiFetch({
+                path: addQueryArgs(
+                    "/mesh_research_kcworks/v1/kcworks-proxy",
+                    queryParams,
+                ),
+            })
+                .then((data) => {
+                    setDataFetched(true);
+                    let a = sortItems(getItems(data));
+                    setResults(a);
+                })
+                .catch((error) => {
+                    console.error(error);
+                    setFetchError(true);
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
+        },
+        [kcworksQuery],
+    );
 
-	function generateBibliography( data ) {
-		const sys = {
-			retrieveLocale: ( locale ) => {
-				if ( Object.hasOwn( locales[ 'primary-dialects' ], locale ) ) {
-					setLocaleSetting( locale );
-					return getXmlFileLocale( locale );
-				} else {
-					return getXmlFileLocale( 'en-US' );
-				}
-			},
-			retrieveItem: ( itemId ) => {
-				return data.find( ( item ) => item.id === itemId );
-			},
-		};
+    useEffect(() => {
+        if (kcworksQuery && validatedKcworksQuery && !dataFetched) {
+            fetchData(
+                setFetchError,
+                setDataFetched,
+                kcworksQuery,
+                setResults,
+                setLoading,
+            );
+        }
+        if (!kcworksQuery) {
+            setLoading(false);
+        }
+    }, [kcworksQuery, validatedKcworksQuery, dataFetched, fetchData]);
 
-		const s = new XMLSerializer();
-		const style = s.serializeToString( getCslFileStyle( styleSetting ) );
-		const citeproc = new CSL.Engine( sys, style );
-		const orcidItemIds = data.map( ( item ) => item.id );
-		citeproc.updateItems( orcidItemIds );
-		const bibliography = citeproc.makeBibliography();
-		setBibliography( bibliography[ 1 ].join( '\n' ) );
-	}
+    function generateBibliography(data) {
+        const sys = {
+            retrieveLocale: (locale) => {
+                if (Object.hasOwn(locales["primary-dialects"], locale)) {
+                    setLocaleSetting(locale);
+                    return getXmlFileLocale(locale);
+                } else {
+                    return getXmlFileLocale("en-US");
+                }
+            },
+            retrieveItem: (itemId) => {
+                return data.find((item) => item.id === itemId);
+            },
+        };
 
-	useEffect( () => {
-		if ( results.length > 0 ) {
-			generateBibliography( results );
-		}
-	}, [ results, styleSetting, sortSetting, localeSetting ] );
+        const s = new XMLSerializer();
+        const style = s.serializeToString(getCslFileStyle(styleSetting));
+        const citeproc = new CSL.Engine(sys, style);
+        const orcidItemIds = data.map((item) => item.id);
+        citeproc.updateItems(orcidItemIds);
+        const bibliography = citeproc.makeBibliography();
+        setBibliography(bibliography[1].join("\n"));
+    }
 
-	function buttonHandler() {
-		setLoading( true );
-		const verification = true; // verifyKcworksQuery( kcworksQuery );
-		setAttributes( { validatedKcworksQuery: true } );
-		if ( ! verification ) {
-			setInvalidQuery( true );
-			setLoading( false );
-		} else {
-			setInvalidQuery( false );
-			fetchData();
-		}
-	}
+    useEffect(() => {
+        if (results.length > 0) {
+            generateBibliography(results);
+        }
+    }, [results, styleSetting, sortSetting, localeSetting]);
 
-	return (
-		<>
-			<DataBlockInspectorControls
-				kcworksQuery={ kcworksQuery }
-				invalidQuery={ invalidQuery }
-				setInvalidQuery={ setInvalidQuery }
-				buttonHandler={ buttonHandler }
-				loading={ loading }
-				setAttributes={ setAttributes }
-				styleSetting={ styleSetting }
-				setStyleSetting={ setStyleSetting }
-				sortSetting={ sortSetting }
-				setSortSetting={ setSortSetting }
-			/>
-			<div { ...useBlockProps() }>
-				{ fetchError && (
-					<div role="alert">
-						<Card>
-							<CardBody>
-								<p>
-									{ __(
-										'An error occurred while fetching the data from KCWorks',
-										'kcworks'
-									) }
-								</p>
-							</CardBody>
-						</Card>
-					</div>
-				) }
-				<p>Welcome to KCWorks</p>
-				{ dataFetched && (
-					<section className="kcworks-bibliography">
-						{
-							<div
-								dangerouslySetInnerHTML={ {
-									__html: bibliography,
-								} }
-							/>
-						}
-					</section>
-				) }
-			</div>
-		</>
-	);
+    function buttonHandler() {
+        setLoading(true);
+        setAttributes({ kcworksQuery: kcworksQuery });
+        const verification = true; // verifyKcworksQuery( kcworksQuery );
+        setAttributes({ validatedKcworksQuery: true });
+        if (!verification) {
+            setInvalidQuery(true);
+            setLoading(false);
+        } else {
+            setInvalidQuery(false);
+            fetchData(
+                setFetchError,
+                setDataFetched,
+                kcworksQuery,
+                setResults,
+                setLoading,
+            );
+        }
+    }
+
+    return (
+        <>
+            <DataBlockInspectorControls
+                kcworksQuery={kcworksQuery}
+                invalidQuery={invalidQuery}
+                setInvalidQuery={setInvalidQuery}
+                buttonHandler={buttonHandler}
+                loading={loading}
+                setAttributes={setAttributes}
+                styleSetting={styleSetting}
+                setStyleSetting={setStyleSetting}
+                sortSetting={sortSetting}
+                setSortSetting={setSortSetting}
+            />
+            <Bibliography
+                fetchError={fetchError}
+                dataFetched={dataFetched}
+                bibliography={bibliography}
+                kcworksQuery={kcworksQuery}
+                loading={loading}
+                results={results}
+            />
+        </>
+    );
 }
