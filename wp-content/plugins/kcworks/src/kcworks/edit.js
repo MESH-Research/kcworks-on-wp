f47133f2f652db1c3ccc26112d0c5e7405080185
Grant Eben <gmeben@gmail.com>
2025-06-25 00:26:33 -0400

    refactor: Extract bibliography logic from edit.js

--- wp-content/plugins/kcworks/src/kcworks/edit.js
+++ wp-content/plugins/kcworks/src/kcworks/edit.js
@@ -15,45 +15,8 @@ import { useCallback, useEffect, useState } from '@wordpress/element';
 import './editor.scss';
 import DataBlockInspectorControls from './components/DataBlockInspectorControls.js';
 import Bibliography from './components/Bibliography.js';
-import CSL from 'citeproc';
-import locales from './locales/locales.json';
 import useCallbackFetch from './useCallbackFetch.js';
-
-const pluginBaseUrl = '/wp-content/plugins/kcworks/src/kcworks';
-
-/**
- * @param {string} styleId
- * @returns
- */
-function getCslFileStyle( styleId ) {
-	if ( styleId === null ) {
-		return '';
-	}
-	const xhr = new XMLHttpRequest();
-	xhr.open( 'GET', `${ pluginBaseUrl }/styles/${ styleId }.csl`, false );
-	xhr.send( null );
-	const text = xhr.responseText;
-	const parser = new DOMParser();
-	return parser.parseFromString( text, 'text/xml' );
-}
-
-/**
- * @param {string} localeId
- * @returns
- */
-function getXmlFileLocale( localeId ) {
-	if ( localeId === null ) {
-		return null;
-	}
-	const xhr = new XMLHttpRequest();
-	xhr.open(
-		'GET',
-		`${ pluginBaseUrl }/locales/locales-${ localeId }.xml`,
-		false
-	);
-	xhr.send( null );
-	return xhr.responseText;
-}
+import { generateBibliography } from './processdata.js';
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -97,33 +60,14 @@ export default function Edit( { attributes, setAttributes } ) {
 		}
 	}, [ kcworksQuery, validatedKcworksQuery, dataFetched ] );
 
-	function generateBibliography( data ) {
-		const sys = {
-			retrieveLocale: ( locale ) => {
-				if ( Object.hasOwn( locales[ 'primary-dialects' ], locale ) ) {
-					setLocaleSetting( locale );
-					return getXmlFileLocale( locale );
-				} else {
-					return getXmlFileLocale( 'en-US' );
-				}
-			},
-			retrieveItem: ( itemId ) => {
-				return data.find( ( item ) => item.id === itemId );
-			},
-		};
-
-		const s = new XMLSerializer();
-		const style = s.serializeToString( getCslFileStyle( styleSetting ) );
-		const citeproc = new CSL.Engine( sys, style );
-		const orcidItemIds = data.map( ( item ) => item.id );
-		citeproc.updateItems( orcidItemIds );
-		const bibliography = citeproc.makeBibliography();
-		setBibliography( bibliography[ 1 ].join( '\n' ) );
-	}
-
 	useEffect( () => {
 		if ( results.length > 0 ) {
-			generateBibliography( results );
+			generateBibliography(
+				results,
+				setLocaleSetting,
+				setBibliography,
+				styleSetting
+			);
 		}
 	}, [ results, styleSetting, sortSetting, localeSetting ] );
 
--- wp-content/plugins/kcworks/src/kcworks/processdata.js
+++ wp-content/plugins/kcworks/src/kcworks/processdata.js
@@ -1,3 +1,6 @@
+import CSL from 'citeproc';
+import locales from './locales/locales.json';
+
 const csl_types = {
 	'article-journal': 'Journal article',
 	book: 'Book',
@@ -11,6 +14,8 @@ const csl_types = {
 	other: 'Other',
 };
 
+const pluginBaseUrl = '/wp-content/plugins/kcworks/src/kcworks';
+
 export function getItems( data ) {
 	return data.hits.hits;
 }
@@ -20,4 +25,67 @@ export function sortItems( data ) {
 		return item;
 	} );
 	return sorted;
+}
+
+/**
+ * @param {string} localeId
+ * @returns
+ */
+function getXmlFileLocale( localeId ) {
+	if ( localeId === null ) {
+		return null;
+	}
+	const xhr = new XMLHttpRequest();
+	xhr.open(
+		'GET',
+		`${ pluginBaseUrl }/locales/locales-${ localeId }.xml`,
+		false
+	);
+	xhr.send( null );
+	return xhr.responseText;
+}
+
+/**
+ * @param {string} styleId
+ * @returns
+ */
+function getCslFileStyle( styleId ) {
+	if ( styleId === null ) {
+		return '';
+	}
+	const xhr = new XMLHttpRequest();
+	xhr.open( 'GET', `${ pluginBaseUrl }/styles/${ styleId }.csl`, false );
+	xhr.send( null );
+	const text = xhr.responseText;
+	const parser = new DOMParser();
+	return parser.parseFromString( text, 'text/xml' );
+}
+
+export function generateBibliography(
+	data,
+	setLocaleSetting,
+	setBibliography,
+	styleSetting
+) {
+	const sys = {
+		retrieveLocale: ( locale ) => {
+			if ( Object.hasOwn( locales[ 'primary-dialects' ], locale ) ) {
+				setLocaleSetting( locale );
+				return getXmlFileLocale( locale );
+			} else {
+				return getXmlFileLocale( 'en-US' );
+			}
+		},
+		retrieveItem: ( itemId ) => {
+			return data.find( ( item ) => item.id === itemId );
+		},
+	};
+
+	const s = new XMLSerializer();
+	const style = s.serializeToString( getCslFileStyle( styleSetting ) );
+	const citeproc = new CSL.Engine( sys, style );
+	const orcidItemIds = data.map( ( item ) => item.id );
+	citeproc.updateItems( orcidItemIds );
+	const bibliography = citeproc.makeBibliography();
+	setBibliography( bibliography[ 1 ].join( '\n' ) );
 }
