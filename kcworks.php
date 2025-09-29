a7f995ddab996d71438422106f613f11a37ab186
Grant Eben <gmeben@gmail.com>
2025-09-28 23:56:52 -0400

    build: Update version to 0.3.0

--- kcworks.php
+++ kcworks.php
@@ -3,7 +3,7 @@
  * Plugin Name:       KCWorks
  * Plugin URI:        https://github.com/MESH-Research/kcworks-wp-plugin
  * Description:       Display results of queries from KCWorks
- * Version:           0.2.0
+ * Version:           0.3.0
  * Requires at least: 6.8.1
  * Requires PHP:      7.4
  * License:           GPL-2.0-or-later
